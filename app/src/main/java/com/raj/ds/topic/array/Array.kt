19e734bee7129cb1b877338c958c1d4a7d2331db
nirajkulal <kulal.niraj@gmail.com>
2025-02-19 22:03:39 +0530

    Binary search code implemented.

---
+++ app/src/main/java/com/raj/ds/topic/array/Array.kt
@@ -0,0 +1,31 @@
+package com.raj.ds.topic.array
+
+import kotlin.Array
+
+fun binarySearch(array: Array<Int>, item: Int) {
+    var mid = array.size / 2
+    var start = 0
+    var end = array.size - 1
+    while (start <= end) {
+        if (array[mid] == item) {
+            println("Item found $item at index $mid")
+            return
+        }
+        if (array[mid] > item) {
+            end = mid - 1
+        } else {
+            start = mid + 1
+        }
+        mid = (start + end) / 2
+    }
+    println("Not found $item")
+
+}
+
+fun main() {
+    println("-------------------------")
+    println("Binary Search:")
+    binarySearch(arrayOf(1, 2, 3, 4, 5, 6, 7), 7)
+    binarySearch(arrayOf(1, 2, 3, 4, 5, 6, 70), 7)
+    println("-------------------------")
+}
