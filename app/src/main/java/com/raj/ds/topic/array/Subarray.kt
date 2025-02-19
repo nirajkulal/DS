052e1253a6387216964a2f41a02ffaf1df3fcb5e
nirajkulal <kulal.niraj@gmail.com>
2025-02-19 22:44:51 +0530

    Largest sum

--- app/src/main/java/com/raj/ds/topic/array/Subarray.kt
+++ app/src/main/java/com/raj/ds/topic/array/Subarray.kt
@@ -2,7 +2,9 @@ package com.raj.ds.topic.array
 
 import com.raj.ds.printDash
 
-
+/**
+ * To print all sub array of given array
+ */
 fun subArray(array: Array<Int>) {
     for (i in 0..array.size - 1) {
         for (j in i..array.size - 1) {
@@ -15,10 +17,30 @@ fun subArray(array: Array<Int>) {
     }
 }
 
+/**
+ * To print largest sum of sub array
+ */
+fun subArrayLargestSumBruteForce(array: Array<Int>) {
+    var sum = 0
+    for (i in 0..array.size - 1) {
+        for (j in i..array.size - 1) {
+            var subSum = 0
+            for (k in i..j) {
+                subSum = subSum + array[k]
+            }
+            if (sum < subSum) {
+                sum = subSum
+            }
+        }
+    }
+    println("Sum : $sum")
+}
+
 fun main() {
     printDash()
     println("Sub array :")
     subArray(arrayOf(1, 2, 3, 4, 5, 6))
     printDash()
-    
+    println("Sub array largest sum by brute force:")
+    subArrayLargestSumBruteForce(arrayOf(1, 2, 3, 4, 5))
 }
